79342fe812333f22621208cf1ff2e3abfa12e1d1
DavidTorres91 <anibal.david.panameno@gmail.com>
2023-09-20 21:59:27 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component'; // Ajusta la ruta si es diferente
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: LandingPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
